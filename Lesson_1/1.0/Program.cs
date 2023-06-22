8a96e6f38e7f50006cea824020840b149d15f0fe
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-22 21:04:23 +0300

    add correction in programm.cs Lesson_1/1.0

--- Lesson_1/1.0/Program.cs
+++ Lesson_1/1.0/Program.cs
@@ -2,6 +2,6 @@
 //string num_str = Console.ReadLine()!;
 
 //int num = int.Parse(num_str);
-int num = int.Parse(Console.ReadLine())!;
+int num = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine($"Квадрат числа равен {num * num}");
