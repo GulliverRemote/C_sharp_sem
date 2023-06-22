eac2087a1755579a967130b82cd1a2e4d2b0ccba
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-22 21:28:47 +0300

    add programm Lesson_1/1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа для сравнения ");
+int num_A = int.Parse(Console.ReadLine()!);
+int num_B = int.Parse(Console.ReadLine()!);
+
+if (num_A/num_B == num_B)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
