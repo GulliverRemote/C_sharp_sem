90bb7e4633a012492d503072361ef440fc72c8a2
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-24 10:25:27 +0300

    feat: add Homework task 8

---
+++ Lesson_1/HW/1_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+
+int a = int.Parse(Console.ReadLine()!); //-6
+int count = 1;// count 6
+while (count != a)
+{
+    if (a > 0)
+        count++;
+    else
+        count--;
+    if (count % 2 == 0)
+        Console.WriteLine(count);
+}
