f8e06ac7f4f5b3b34847fbde99607b9936a8a5c5
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-24 09:54:21 +0300

    feat: add Home work task 2

---
+++ Lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 2 числа");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a>b)
+Console.WriteLine($"MAX = {a} ; MIN = {b}");
+else if (b>a)
+Console.WriteLine($"MAX = {b} ; MIN = {a}");
+else
+Console.WriteLine($"Первое число a = {a} равно второму числу b = {b}");
