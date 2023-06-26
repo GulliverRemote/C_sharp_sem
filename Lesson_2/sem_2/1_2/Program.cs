6d2632ec72963515216913f6abd7a86edc3873de
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-26 21:27:12 +0300

    feat: add sem_2 task 3

---
+++ Lesson_2/sem_2/1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿void Ost(int a, int b)
+{
+    if (a % b == 0)
+        Console.WriteLine($"{a} и {b} кратны");
+    else
+    Console.WriteLine($"{a} и {b} не кратны, остаток {a % b}");
+}
+
+int first = int.Parse(Console.ReadLine()!),
+    sec = int.Parse(Console.ReadLine()!);
+Ost(first, sec);
