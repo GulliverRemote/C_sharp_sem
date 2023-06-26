1083431be64aa7830140e325c0eb6a4c69cb3a0c
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-26 21:10:44 +0300

    feat: add sem_2 task 2 another

--- Lesson_2/sem_2/1_1/Program.cs
+++ Lesson_2/sem_2/1_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿int Num(int num)
 {
     Console.WriteLine(num);
+    /*
     int c = num % 10,
         d = num / 10,
         res;
@@ -12,7 +13,11 @@
     {
         res = d;
     }
-    return res;
+    return res;*/
+
+    if (num / 10 > num % 10)
+        return num / 10;
+    return num % 10;
 }
 
 int a = new Random().Next(10, 100);
