f641a943d9086d193dab59882038543937bbdec3
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-26 20:49:32 +0300

    feat: add sem_2 task 1

---
+++ Lesson_2/sem_2/1_0/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Последняя цифра в этом числе - " + a % 10);
+*/
+
+
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int a = new Random().Next(100,1000);
+int result = TakeNum(a);
+Console.WriteLine(result); 
