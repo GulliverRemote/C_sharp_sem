11aee0423a132cbf14abbd4cf2fab97ed9c9c9aa
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-28 20:00:17 +0300

    feat: add Lesson_2 Homework task 1

---
+++ Lesson_2/HW/1_0/Program.cs
@@ -0,0 +1,12 @@
+﻿int Sec(int num)
+{
+    Console.WriteLine(num);
+    if (num > 0)
+        return (num / 10) % 10;
+    return Math.Abs(num / 10) % 10;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int three = int.Parse(Console.ReadLine()!),
+    Out = Sec(three);
+Console.WriteLine($"Вторая цифра числа равна {Out}");
