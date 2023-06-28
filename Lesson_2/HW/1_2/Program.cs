13042cf04b910eda30212013d065bcb54973d31c
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-28 21:13:31 +0300

    feat: add Lesson_2 Homework task 3

---
+++ Lesson_2/HW/1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿void DayOff(int day)
+{
+    if (day == 1)
+        Console.WriteLine("Понедельник - день тяжелый, работаем");
+    else if (day == 2)
+        Console.WriteLine("Вторник - отдых от понедельника, работаем");
+    else if (day == 3)
+        Console.WriteLine("Среда - половина рабочей недели");
+    else if (day == 4)
+        Console.WriteLine("Четверг - подготовка к пятнице, работаем");
+    else if (day == 5)
+        Console.WriteLine("Пятница - немного и в отрыв, а пока дорабатываем");
+    else if (day == 6)
+        Console.WriteLine("Суббота - продолжаем пятничную радость, ВЫХОДНОЙ!!!");
+    else if (day == 7)
+        Console.WriteLine(
+            "Воскресенье - отходим от радости, готовимся к понедельнику =( ВЫХОДНОЙ "
+        );
+    else
+        Console.WriteLine("Нет такого дня?");
+}
+
+Console.WriteLine("Введите день недели цифрой");
+int a = int.Parse(Console.ReadLine()!);
+DayOff(a);
