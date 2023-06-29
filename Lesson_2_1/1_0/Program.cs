01b4cf671e6c9e349cb2582a1c1871fe5e637cb2
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-29 20:43:32 +0300

    feat: add Lesson_2_1 sem 2_1 task 1

--- Lesson_2_1/1_0/Program.cs
+++ Lesson_2_1/1_0/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+*/
+
+
+
+void Chetv(int x, int y)
+{
+    Console.WriteLine($"Координаты точки ({x}, {y})");
+    if (x == 0 || y == 0)
+        Console.WriteLine("Точка на оси");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("Точка находится в 1 четверти");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("Точка находится в 2 четверти");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("Точка находится в 3 четверти");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("Точка находится в 4 четверти");
+}
+
+Console.WriteLine("Введите координаты точки");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Chetv(num1, num2);
