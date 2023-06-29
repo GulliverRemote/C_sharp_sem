5d68e14bdc1f92bd606d5778db1f83f2d15de542
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-29 22:06:54 +0300

    feat: add Lesson_2_1 sem 2_1 task 4

---
+++ Lesson_2_1/1_3/Program.cs
@@ -0,0 +1,27 @@
+﻿void Kvadr(int num, int step)
+{
+    int n = 1;
+    if (num > 0)
+    {
+        while (n < num + 1)
+        {
+            Console.Write($"{Math.Pow(n, step)} ");
+            n += 1;
+        }
+    }
+    else
+        while (n > num - 1)
+        {
+            Console.Write($"{Math.Pow(n, step)} ");
+            n -= 1;
+            if (n == 0)
+            {
+                Console.Write("no ");
+                n -= 1;
+            }
+        }
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+Kvadr(a, a);
