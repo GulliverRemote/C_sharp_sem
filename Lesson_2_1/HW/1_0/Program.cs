5789babe8d210fd7985b020bc291e6f563567e7a
Gulliver <shipulinshshsh2000@gmail.com>
2023-06-29 23:40:25 +0300

    refactor: fixed spelling error

--- Lesson_2_1/HW/1_0/Program.cs
+++ Lesson_2_1/HW/1_0/Program.cs
@@ -4,9 +4,9 @@ void Polin(int num)
     if (num > 10000 && num < 100000)
     {
     if ((num/10000 == num%10) && ((num/1000)%10)==(num/10)%10)
-    Console.WriteLine($"Число {num} полином");
+    Console.WriteLine($"Число {num} полиндром");
     else
-    Console.WriteLine($"Число {num} не является полиномом");
+    Console.WriteLine($"Число {num} не является полиндромом");
     }
     else
     Console.WriteLine("Некорректное число");
