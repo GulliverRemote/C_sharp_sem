3aa29d1e760401f1542cf89ab879f114abeb1ed8
GulliverRemote <136632155+GulliverRemote@users.noreply.github.com>
2023-06-29 23:32:19 +0300

    Revert "solve mistake"

---
+++ Lesson_2/HW/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int Third(int num)
+{
+    Console.WriteLine(num);
+    num = Math.Abs(num);
+    if (num < 100)
+        return -1;
+    while ((num) > 999)
+        num = num / 10;
+    return num % 10;
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!),
+    res = Third(a);
+if (res == -1)
+    Console.WriteLine("В данном числе нет третей цифры");
+else
+    Console.WriteLine($"Третья цифра - {res}");
