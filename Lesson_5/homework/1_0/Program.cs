14e333f1754af0474da311a5935f71ed702451a5
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-08 18:11:00 +0300

    feat: add Lesson_5 homework task 1

---
+++ Lesson_5/homework/1_0/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] FillMas(int n, int min, int max)
+{
+    int[] Mas = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Mas[i] = new Random().Next(min, max + 1);
+    }
+    return Mas;
+}
+
+void PrintMas(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0) count++;
+    }
+    Console.WriteLine($"Количество четных чисел равно {count}");
+}
+
+Console.Write("Введите число элементов массива : ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элемента массива : ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int stop = int.Parse(Console.ReadLine()!);
+int[] arr = FillMas(num, start, stop);
+PrintMas(arr);
+CountEven(arr);
