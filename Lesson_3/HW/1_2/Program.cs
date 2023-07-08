dc791619298ffb60bce5486571986a632c0070fa
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-08 10:03:30 +0300

    feat: add Lesson_3 Homework task 3 and version

---
+++ Lesson_3/HW/1_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] FillMas(int n) //заполнение массива с клавиатуры
+{
+    int[] Mas = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"\nВведите элемент массива [{i}] : ");
+        Mas[i] = int.Parse(Console.ReadLine()!);
+    }
+    return Mas;
+}
+
+void PrintMas(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите число элементов массива");
+int num = int.Parse(Console.ReadLine()!);
+int[] Arr = FillMas(num);
+PrintMas(Arr);
