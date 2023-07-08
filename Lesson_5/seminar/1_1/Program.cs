9b1a87cd51b9090f594ec2dd697cb2c14fd4ba41
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-08 15:56:31 +0300

    feat: add Lesson_5 seminar task 2

---
+++ Lesson_5/seminar/1_1/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] FillMas(int n, int min, int max)
+{
+    int[] Mas = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Mas[i] = new Random().Next(min, max + 1);
+    }
+    return Mas;
+}
+
+void PrintMas(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] ChangeNegatPosit(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= (-1);
+    }
+    return array;
+}
+
+Console.Write("Введите число элементов массива : ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элемента массива : ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int stop = int.Parse(Console.ReadLine()!);
+int[] arr = FillMas(num, start, stop);
+PrintMas(arr);
+int[] newArr = ChangeNegatPosit(arr);
+PrintMas(newArr);
