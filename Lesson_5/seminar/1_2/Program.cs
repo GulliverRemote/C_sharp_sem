b604e98bf9586fc84c9eb2609a5559793316a243
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-08 16:15:11 +0300

    feat: add Lesson_5 seminar task 3

---
+++ Lesson_5/seminar/1_2/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] FillMas(int n, int min, int max)
+{
+    int[] Mas = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Mas[i] = new Random().Next(min, max + 1);
+    }
+    return Mas;
+}
+
+void PrintMas(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void IfDigit(int[] array)
+{
+    Console.WriteLine("Введите искомое число : ");
+    int num = int.Parse(Console.ReadLine()!);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num)
+        {
+            Console.WriteLine($"Число {num} присутствует");
+            return;
+        }
+    }
+    Console.WriteLine($"Числа {num} нет");
+    return;
+}
+
+Console.Write("Введите число элементов массива : ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элемента массива : ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int stop = int.Parse(Console.ReadLine()!);
+int[] arr = FillMas(num, start, stop);
+PrintMas(arr);
+IfDigit(arr);
