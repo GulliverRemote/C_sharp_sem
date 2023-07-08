5367d6156eb42fd02b1b0396094df08fa4e2c4d7
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-08 18:25:14 +0300

    feat: add Lesson_5 homework task 2

---
+++ Lesson_5/homework/1_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillMas(int n, int min, int max)
+{
+    int[] Mas = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Mas[i] = new Random().Next(min, max + 1);
+    }
+    return Mas;
+}
+
+void PrintMas(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int SumEven(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i ++)
+    {
+        if (array[i] % 2 == 0)
+            sum += array[i];
+    }
+    return sum;
+}
+
+Console.Write("Введите число элементов массива : ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элемента массива : ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int stop = int.Parse(Console.ReadLine()!);
+int[] arr = FillMas(num, start, stop);
+PrintMas(arr);
+Console.WriteLine("Сумма четных элементов равна " + SumEven(arr));
