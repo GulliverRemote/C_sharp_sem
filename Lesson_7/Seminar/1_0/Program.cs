5639915f0f808c0d1b38fe594d47077726c2aa52
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-13 20:43:56 +0300

    feat: add Lesson_7 Seminar task 1

--- Lesson_7/Seminar/1_0/Program.cs
+++ Lesson_7/Seminar/1_0/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размера m на n, каждый
+// элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i+j;
+        }
+    }
+}
+
+
+
+Console.WriteLine("Введите количество столбцов ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество строк ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
