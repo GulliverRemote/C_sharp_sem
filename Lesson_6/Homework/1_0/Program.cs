5b901dad149fe2281f0ddd53f528192a67572806
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-13 19:37:08 +0300

    feat: add Lesson_6 Homework task 1

---
+++ Lesson_6/Homework/1_0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int CountMoreZero()
+{
+    int count = 0;
+    double num = 0;
+    Console.WriteLine("Введите числа, стоп число -1 : ");
+    while (num != -1)
+    {
+        num = double.Parse(Console.ReadLine()!);
+        if (num > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine($"Чисел больше 0 = {CountMoreZero()}");
