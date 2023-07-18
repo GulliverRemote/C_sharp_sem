ef720b0006da1535d6f5ba0046248e72b08bacb4
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-18 18:35:13 +0300

    feat: add Lesson_7 Homework task 1

---
+++ Lesson_7/Homework/1_0/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j], 5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    Console.Write("Введите минимальный элемент :");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите максимальный элемент :");
+    int max = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Math.Round ((new Random().NextDouble() * (max - min) + min),4);
+            
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов ");
+int n = int.Parse(Console.ReadLine()!);
+double[,] matrix = new double[m, n];
+FillArray(matrix);
+PrintArray(matrix);
