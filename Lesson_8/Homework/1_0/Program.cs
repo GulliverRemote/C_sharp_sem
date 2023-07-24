f6ffe58a5256beebda7957d8efbe6d1472443bc7
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-24 19:00:48 +0300

    feat: add Lesson_8 Homework task 1

---
+++ Lesson_8/Homework/1_0/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] PrintArray(int[,] matr)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j], 3} ");
+        }
+        Console.WriteLine();
+    }
+    return matr;
+}
+
+int[,] FillArray(int[,] matr)
+{
+    Console.Write("Введите минимальное значение элемента массива : ");
+    int min = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите максимальное значение элемента массива : ");
+    int max = int.Parse(Console.ReadLine()!);
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void DescendingSort(int[,] array)
+{
+    bool stop;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        do
+        {
+            stop = false;
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    (array[i, j], array[i, j + 1]) = (array[i, j + 1], array[i, j]);
+                    stop = true;
+                }
+            }
+        } while (stop != false);
+    }
+}
+
+Console.WriteLine("Введите количество строк ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
+DescendingSort(matrix);
+PrintArray(matrix);
