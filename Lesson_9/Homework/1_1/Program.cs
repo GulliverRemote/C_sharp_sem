7a5a823410ab87d38e120c21a552968d204ff4c9
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-26 17:40:26 +0300

    feat: add Lesson_9 Homework task 2

---
+++ Lesson_9/Homework/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int SumDigits(int numFirst, int numSecond)
+{
+    if (numFirst == numSecond)
+        return numFirst;
+    return SumDigits(numFirst + 1, numSecond) + numFirst;
+}
+
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(SumDigits(n, m));
