0850176ac16026f03837e4579abfb590d46b0523
Gulliver <shipulinshshsh2000@gmail.com>
2023-07-26 16:34:28 +0300

    feat: add Lesson_9 Seminar task 2

---
+++ Lesson_9/Seminar/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void IntegerNumbers(int a, int b) 
+{
+    if (a == b+1) return;
+    Console.Write(a + " ");
+    IntegerNumbers(a + 1, b);
+    
+}
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine()!);
+
+IntegerNumbers(n, m);
